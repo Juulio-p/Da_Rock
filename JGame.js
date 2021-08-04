3cd7e1ab439e276c65eb54a3dd9ce5540a4efac4
Julio prudencio <pjulio1234@gmail.com>
2021-08-03 23:31:23 -0700

    added scoreboard,and displayed it also made the buttons get disabled after certain amount of clicks

---
+++ JGame.js
@@ -0,0 +1,109 @@
+
+const buttons = document.querySelectorAll('button')
+let computerScore =0;
+let playerScore= 0;
+
+
+
+
+
+buttons.forEach((button) => {
+        
+    button.addEventListener('click', () =>{
+
+    playerSelection = button.id;
+
+    playRound(playerSelection,computerSelection);
+    if(playerScore +computerScore >=5){
+        button.disabled =true;
+        if(playerScore >computerScore){
+        document.getElementById('winner').innerHTML = "You Win! refresh page to play again";
+        }else if(playerScore< computerScore){
+            document.getElementById('winner').innerHTML = "You Loose , better luck next time! refresh page to play again";
+        }
+
+}
+    });
+
+});
+
+
+const computerSelection = computerPlay();
+// computer selection passed on to another function, computer play.
+    function computerPlay() {
+        const choices = ["rock", "paper", "scissors"];
+        // array of three choices
+        let index = Math.floor(Math.random() * choices.length);
+        // picks ranom number, within th array "choices" length
+        let computerSelection = choices[index];
+        /* declared coomputerSelection to be any of
+         the random indexes from the array"choices"*/
+        return computerSelection;
+    }
+
+    function win(playerSelection, computerSelection){
+        let compute= playerSelection.length - computerSelection.length ;
+        /* compares lengths of the Strings. one is gotten from the user and another
+         from the function computer play */
+        let winner=false;
+        // made boolean expression start with false;
+        if(compute>0 ||compute==-4 ){
+            // conditions to be met in order for the win to be true
+            winner= true;
+        }else if(compute<0 || compute==4) {
+            winnner= false;
+            // i guess keep the variable the same could inproved but works perfectly
+        }
+        return winner; 
+    }
+
+
+
+    function playRound(playerSelection, computerSelection) {
+        let statment = "";
+        let tie = playerSelection == computerSelection;
+        if (tie) {
+            statment += "Tie, both picked " + computerSelection;
+            // if both player and computer had the same results just print them and say they the same
+        } else if(win(playerSelection,computerSelection)){
+            // pass through win function 
+            statment += "you win " + playerSelection+ " beats " +computerSelection;
+            // if win comes back true reeturn this statement
+        }else if(!(win(playerSelection,computerSelection))){
+            // if the win function retruns false print this 
+            statment += "you lose "+ computerSelection +" beats " + playerSelection;
+        }
+
+        if(win(playerSelection, computerSelection)){
+            playerScore +=1;
+        }else if (!(win(playerSelection, computerSelection) || (tie))){
+            computerScore +=1;
+        } else if(!(win(playerSelection, computerSelection)) && tie ){
+
+        }
+
+       let scoreBoard = '<br>' +'PlayerScore: ' + playerScore + ' <br>  Computer Score: ' + computerScore;
+
+        document.getElementById('Results').innerHTML= statment  + scoreBoard;
+
+        
+        return statment;
+    }
+
+   
+/*
+function game(playerSelection,computerSelection){
+    for (let i = 0; i < 5; i++) {// looping through game 5 times
+      
+        playerSelection.toLowerCase(); 
+        // making the comparison Lower case so function can read regardless of punctuation
+        playRound(playerSelection,computerSelection);
+        // calling the game 
+
+         i+1;
+    }
+}
+    */
+
+
+
